c1d3b41f386db41e8876047fbcf30ef76777ee7c
julieprunaret <j.prunaret@gmail.com>
2022-06-08 18:04:00 +0200

    form creation

--- my-immo/src/Controller/HomeController.php
+++ my-immo/src/Controller/HomeController.php
@@ -13,7 +13,8 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(ManagerRegistry $doctrine): Response
     {
-        $biens = $doctrine->getRepository(Biens::class)->findAll();
+        //$biens = $doctrine->getRepository(Biens::class)->findAll();
+        $biens = $doctrine->getRepository(Biens::class)->findBy([], ['id' => 'DESC']);
         return $this->render('home/index.html.twig', [
             'biens' => $biens 
         ]);
--- my-immo/src/Controller/ProductPageController.php
+++ my-immo/src/Controller/ProductPageController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use App\Entity\Biens;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductPageController extends AbstractController
 {
-    #[Route('/product/page', name: 'app_product_page')]
-    public function index(): Response
+    #[Route('/bien/details/{id}', name: 'product')]
+    public function details($id, ManagerRegistry $doctrine): Response
     {
-        return $this->render('product_page/index.html.twig', [
-            'controller_name' => 'ProductPageController',
+        // Récupère l'objet en fonction de l'@Id (généralement appelé $id)
+        $bien = $doctrine->getRepository(Biens::class)->find($id);
+        // vérifier $bien avec le var dump => if($bien){var dump}else{erreur}
+        return $this->render('product_page/details.html.twig', [
+            'bien' => $bien
         ]);
     }
+
+    #[Route('/bien/add', name: 'add_product')]
+    public function add()
+    {
+        //etape 1 : à la création, on instancie une objet vide
+        $bien = new Biens();
+        //vous pouvez setter les valeurs par défaut de l'objet
+        $bien->setCreatedAt(new DateTime()); //donne la date du jour
+
+        // METHODE 1 : création du formulaire directement en controller
+        $formBien = $this->createFormBuilder($bien)
+        ->add('title', TextType::class)
+        ->add('prix', IntegerType::class)
+        ->add('description', TextAreaType::class)
+        ->add('city', TextType::class)
+        ->getForm();
+
+        return $this->render('product_page/form-add.html.twig');
+    }
+
 }
