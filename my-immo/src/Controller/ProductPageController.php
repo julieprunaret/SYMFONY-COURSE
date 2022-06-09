97e9ef25f1465eb1f7739dfe50ccb0cd99b95ab0
julieprunaret <j.prunaret@gmail.com>
2022-06-09 13:49:40 +0200

    create

--- my-immo/src/Controller/ProductPageController.php
+++ my-immo/src/Controller/ProductPageController.php
@@ -4,12 +4,15 @@ namespace App\Controller;
 
 use DateTime;
 use App\Entity\Biens;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use App\Form\FormBienType;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductPageController extends AbstractController
@@ -20,28 +23,70 @@ class ProductPageController extends AbstractController
         // Récupère l'objet en fonction de l'@Id (généralement appelé $id)
         $bien = $doctrine->getRepository(Biens::class)->find($id);
         // vérifier $bien avec le var dump => if($bien){var dump}else{erreur}
-        return $this->render('product_page/details.html.twig', [
+        return $this->render('product_page/details.html.twig', [ //le render envoit l'affichage
             'bien' => $bien
         ]);
     }
 
-    #[Route('/bien/add', name: 'add_product')]
-    public function add()
+    #[Route('/bien/add', name: 'add_product')] 
+    /* 
+    *le premier url est celui que l'on défini pour la page concernée.
+    *le name est le nom que l'on donne à la page (son id, qui sera dans le path des liens)
+    */
+    public function add(Request $request, ManagerRegistry $doctrine)
     {
-        //etape 1 : à la création, on instancie une objet vide
-        $bien = new Biens();
-        //vous pouvez setter les valeurs par défaut de l'objet
-        $bien->setCreatedAt(new DateTime()); //donne la date du jour
+        
+        $bien = new Biens();//etape 1 : à la création, on instancie une objet vide
+        $bien->setCreatedAt(new DateTime()); //ici on donne les champs remplis par défaut, ils n'apparaitront pas dans le formulaire => donne la date du jour => ce champs sera rempli automatiquement
 
-        // METHODE 1 : création du formulaire directement en controller
-        $formBien = $this->createFormBuilder($bien)
-        ->add('title', TextType::class)
-        ->add('prix', IntegerType::class)
-        ->add('description', TextAreaType::class)
-        ->add('city', TextType::class)
-        ->getForm();
 
-        return $this->render('product_page/form-add.html.twig');
+        // METHODE 1 (FORM BUILDER) : création du formulaire directement en controller
+
+
+        // $formBien = $this->createFormBuilder($bien) //on crée un formulaire à la main (sans la CLI)
+        // ->add('title', TextType::class)//on donne le nom du champs puis son type en HTML (et donc text pas string), on peut mettre les contraintes ici en 3em parametre (pour les passwords et email par exemple)
+        // ->add('prix', IntegerType::class)
+        // ->add('description', TextAreaType::class)
+        // ->add('city', TextType::class)
+        // //->add('save', SubmitType::class, ['label' => 'Enregistrer'] )//dans le cadre d'un formulaire personnalisé on gère le input dans la view
+        // ->getForm(); //on récupère le formulaire après avoir nommé ses champs
+
+
+
+        
+        // METHODE 2 (FORM BUILDER) : création du formulaire avec la commande
+        $formBien = $this->createForm(FormBienType::class, $bien); //tout est géré dans le form
+
+        $formBien->handleRequest($request); //on récupère la requete
+        
+        //on teste ensuite si le formulaire 1) a été rempli et soumi et 2) s'il est valide
+        if($formBien->isSubmitted() && $formBien->isValid())
+        {
+            //l'entité manager de doctrine vous permettra d'enregistrer les données en bdd
+            $entityManager = $doctrine->getManager();
+
+            $entityManager->persist($bien);//on enregistre de nouvelles données
+            $entityManager->flush();
+
+            //pour ajouter un message dans le feedback; dans la variable 'flash'
+            $this->addFlash(
+                'success_add', 
+                'Le bien a été ajouté !'
+            );
+
+            return $this->redirectToRoute('app_home');//on peut envoyer à la fin sur home ou une autre page
+        }
+
+
+        //méthod render 1
+        return $this->render('product_page/form-add.html.twig', [ //le render envoit l'affichage
+            'formBien' => $formBien->createView()
+        ]);// ici on crée la vue du formulaire
+
+        //méthode render 2 (nouvelle)
+        // return $this->renderForm('product_page/form-add.html.twig', [
+        //     'formBien' => $formBien
+        // ]);
+
     }
-
 }
---
+++ my-immo/src/Form/FormBienType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Biens;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormBienType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'required' => true,
+                'label' => 'Mon titre'
+            ]) 
+            ->add('prix', IntegerType::class, [
+                'required' => false
+            ])
+            ->add('description', TextareaType::class, [
+                'required' => false
+            ])
+            ->add('city', TextType::class, [
+                'required' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Biens::class,
+        ]);
+    }
+}
